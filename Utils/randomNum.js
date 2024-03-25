b11a1461e374188d6060929b0193de2d4ca40fe1
Abhirami V V <abhiramivv77@gmail.com>
2024-03-25 18:00:53 +0530

    Random  number return function updated

--- Utils/randomNum.js
+++ Utils/randomNum.js
@@ -1,5 +1,6 @@
 const randomNumber = () => {
-    return  Math.floor(1000 + Math.random() * 9000);
+    return Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000;
+    // return  Math.floor(1000 + Math.random() * 9000);
 };
 
 export { randomNumber };
